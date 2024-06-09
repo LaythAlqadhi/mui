1928b8a288242c9c29e1cfb1665cad2b4800f40b
Layth Alqadhi <125077045+LaythAlqadhi@users.noreply.github.com>
2024-06-09 17:03:09 +0300

    routing

--- mui/app/page.jsx
+++ mui/app/page.jsx
@@ -1,16 +1,27 @@
 import TourCard from '@/app/ui/tourCard';
-import { Grid } from '@mui/material';
 import { Container } from '@mui/material';
+import SearchAppBar from './ui/AppBar';
+import cities from '@/app/lib/data';
+import { Grid, Typography } from '@mui/material';
 
 export default function Page() {
   return (
-    <Container>
-      <Grid container spacing={5}>
-        <TourCard />
-        <TourCard />
-        <TourCard />
-        <TourCard />
-      </Grid>
-    </Container>
+    <div>
+      <SearchAppBar />
+      <Container>
+        {cities.map((city) => (
+          <>
+            <Typography variant="caption" component="p">
+              Top {city.name} tours
+            </Typography>
+            <Grid container spacing={5} paddingY={5}>
+              {city.tours.map((tour) => (
+                <TourCard tour={tour} />
+              ))}
+            </Grid>
+          </>
+        ))}
+      </Container>
+    </div>
   );
 }
--- mui/app/ui/tourCard.jsx
+++ mui/app/ui/tourCard.jsx
@@ -1,17 +1,66 @@
-import { Grid } from '@mui/material';
+import { Box, Grid, Rating, Typography } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import Image from 'next/image';
+import { AccessAlarm } from '@mui/icons-material';
 
-export default function TourCard() {
+export default function TourCard({ tour }) {
   return (
     <Grid item xs={3}>
       <Paper elevation={2} square>
         <Image
           src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
           alt=""
-          width={500}
-          height={500}
+          height={358}
+          width={626}
+          style={{ width: '100%', height: '5rem' }}
         />
+        <Box paddingX={1}>
+          <Typography variant="subtitle1" component="h2">
+            {tour.name}
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              gap: 0.5,
+            }}
+          >
+            <AccessAlarm
+              sx={{
+                width: 12.5,
+              }}
+            />
+            <Typography variant="caption" component="p">
+              {tour.duration} hours
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              marginTop: 1.5,
+              gap: 0.5,
+            }}
+          >
+            <Rating
+              name="read-only"
+              value={tour.rating}
+              precision={0.5}
+              size="small"
+              readOnly
+            />
+            <Typography variant="body2" component="p">
+              {tour.rating}
+            </Typography>
+            <Typography variant="caption" component="p">
+              ({tour.numberOfReviews} reviews)
+            </Typography>
+          </Box>
+          <Typography variant="subtitle2" component="h3">
+            From C ${tour.price}
+          </Typography>
+        </Box>
       </Paper>
     </Grid>
   );
